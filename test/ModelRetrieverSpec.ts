07127898ebf36047262fa10341b28c110623a8a2
Mattia Franchetto <mt.franchetto@gmail.com>
2017-01-25 15:24:16 +0100

    Remove sinon in favour of typemoq

--- test/ModelRetrieverSpec.ts
+++ test/ModelRetrieverSpec.ts
@@ -1,12 +1,10 @@
 import "reflect-metadata";
 import expect = require("expect.js");
-import sinon = require("sinon");
 import Rx = require("rx");
-import NotificationManager from "../scripts/notifications/NotificationManager";
+import * as TypeMoq from "typemoq";
 import IModelRetriever from "../scripts/model/IModelRetriever";
 import {IHttpClient} from "ninjagoat";
 import INotificationManager from "../scripts/notifications/INotificationManager";
-import MockSocketClient from "./fixtures/MockSocketClient";
 import ModelRetriever from "../scripts/model/ModelRetriever";
 import ModelState from "../scripts/model/ModelState";
 import TestCounter from "./fixtures/TestCounter";
@@ -14,54 +12,61 @@ import {ViewModelContext} from "ninjagoat";
 import ModelPhase from "../scripts/model/ModelPhase";
 import {HttpResponse} from "ninjagoat";
 import MockHttpClient from "./fixtures/MockHttpClient";
+import MockNotificationManager from "./fixtures/MockNotificationManager";
 
 describe("Model retriever, given an area and a viewmodel id", () => {
 
-    let subject:IModelRetriever;
-    let httpClient:IHttpClient;
-    let notificationManager:INotificationManager;
-    let sandbox:sinon.SinonSandbox;
-    let socketClient:SocketIOClient.Socket;
+    let subject: IModelRetriever;
+    let httpClient: TypeMoq.IMock<IHttpClient>;
+    let notificationManager: TypeMoq.IMock<INotificationManager>;
 
     beforeEach(() => {
-        sandbox = sinon.sandbox.create({
-            useFakeTimers: true
+        httpClient = TypeMoq.Mock.ofType(MockHttpClient);
+        notificationManager = TypeMoq.Mock.ofType(MockNotificationManager);
+        subject = new ModelRetriever(httpClient.object, notificationManager.object);
+        notificationManager.setup(n => n.notificationsFor(TypeMoq.It.isAny())).returns(context => {
+            return Rx.Observable.just({url: 'http://testurl/' + (context.parameters ? context.parameters.id : "")});
         });
-        socketClient = new MockSocketClient();
-        httpClient = new MockHttpClient();
-        notificationManager = new NotificationManager(socketClient);
-        subject = new ModelRetriever(httpClient, notificationManager);
+        httpClient.setup(h => h.get("http://testurl/")).returns(() => {
+            return Rx.Observable.just(new HttpResponse({count: 20}, 200));
+        });
+        httpClient.setup(h => h.get("http://testurl/60")).returns(() => {
+            return Rx.Observable.just(new HttpResponse({count: 60}, 200));
+        });
     });
 
     context("when a viewmodel needs data to be loaded", () => {
-
-        beforeEach(stubData);
-
         it("should send a loading state to the viewmodel", () => {
-            let modelState:ModelState<TestCounter> = null;
+            let modelState: ModelState<TestCounter> = null;
             subject.modelFor<TestCounter>(new ViewModelContext("Admin", "Bar")).take(1).subscribe(item => modelState = item);
-            sandbox.clock.tick(10);
             expect(modelState.phase).to.be(ModelPhase.Loading);
         });
     });
 
     context("when a loading state has been sent to the viewmodel", () => {
-
-        beforeEach(stubData);
-
         it("should load the data", () => {
-            let modelState:ModelState<TestCounter> = null;
+            let modelState: ModelState<TestCounter> = null;
             subject.modelFor<TestCounter>(new ViewModelContext("Admin", "Bar")).skip(1).take(1).subscribe(item => modelState = item);
-            sandbox.clock.tick(10);
             expect(modelState.model.count).to.be(20);
         });
 
         context("and some parameters are needed to construct the model", () => {
             it("should append those parameters when requesting the model", () => {
-                let modelState:ModelState<TestCounter> = null;
+                let modelState: ModelState<TestCounter> = null;
                 subject.modelFor<TestCounter>(new ViewModelContext("Admin", "Bar", {id: 60})).skip(1).take(1).subscribe(item => modelState = item);
-                sandbox.clock.tick(10);
                 expect(modelState.model.count).to.be(60);
+            });
+        });
+
+        context("and some parameters are needed on the backend side", () => {
+            it("should pass those parameters in the query string", () => {
+
+            });
+        });
+
+        context("and parameters aren't needed on the backend side", () => {
+            it("should keep the query string empty", () => {
+
             });
         });
     });
@@ -69,30 +74,16 @@ describe("Model retriever, given an area and a viewmodel id", () => {
     context("if something bad happens while retrieving the data needed by the viewmodel", () => {
 
         beforeEach(() => {
-            sandbox.stub(notificationManager, "notificationsFor", (context:ViewModelContext) => {
-                return Rx.Observable.just({url: 'http://testurl/' + (context.parameters ? context.parameters.id : "")});
+            httpClient.reset();
+            httpClient.setup(h => h.get(TypeMoq.It.isAny())).returns(() => {
+                return Rx.Observable.throw<HttpResponse>({message: 'Something bad happened'});
             });
-            sandbox.stub(httpClient, "get", (url:string) => Rx.Observable.throw({message: 'Something bad happened'}));
         });
 
         it("should push a failed state to the viewmodel", () => {
-            let modelState:ModelState<TestCounter> = null;
+            let modelState: ModelState<TestCounter> = null;
             subject.modelFor<TestCounter>(new ViewModelContext("Admin", "Bar", {id: 60})).skip(1).take(1).subscribe(item => modelState = item);
-            sandbox.clock.tick(10);
             expect(modelState.failure).to.eql({message: 'Something bad happened'});
         });
     });
-
-    function stubData() {
-        sandbox.stub(notificationManager, "notificationsFor", (context:ViewModelContext) => {
-            return Rx.Observable.just({url: 'http://testurl/' + (context.parameters ? context.parameters.id : "")});
-        });
-        sandbox.stub(httpClient, "get", (url:string) => {
-            if (url === 'http://testurl/') {
-                return Rx.Observable.just(new HttpResponse({count: 20}, 200));
-            } else if (url === 'http://testurl/60') {
-                return Rx.Observable.just(new HttpResponse({count: 60}, 200));
-            }
-        });
-    }
 });
--- test/NotificationManagerSpec.ts
+++ test/NotificationManagerSpec.ts
@@ -1,50 +1,39 @@
 import "reflect-metadata";
 import expect = require("expect.js");
-import sinon = require("sinon");
+import * as TypeMoq from "typemoq";
 import NotificationManager from "../scripts/notifications/NotificationManager";
-import * as Rx from "rx";
 import MockSocketClient from "./fixtures/MockSocketClient";
 import {ViewModelContext} from "ninjagoat";
 
 describe("NotificationManager, given an area and a viewmodel id", () => {
 
-    let subject:NotificationManager;
-    let client:SocketIOClient.Socket;
-    let emitStub:sinon.SinonStub;
-    let notificationsStreamStub:sinon.SinonStub;
+    let subject: NotificationManager;
+    let client: TypeMoq.IMock<SocketIOClient.Socket>;
 
     beforeEach(() => {
-        client = new MockSocketClient();
-        subject = new NotificationManager(client);
-        emitStub = sinon.stub(client, "emit");
-        notificationsStreamStub = sinon.stub(subject, "getNotificationStream", () => {
-            return Rx.Observable.just({url: 'test'});
-        });
+        client = TypeMoq.Mock.ofType(MockSocketClient);
+        subject = new NotificationManager(client.object);
     });
 
-    afterEach(() => {
-        notificationsStreamStub.restore();
-        emitStub.restore();
-    });
 
     context("when this viewmodel needs notifications about the model change", () => {
         it("should subscribe to the backend", () => {
             subject.notificationsFor(new ViewModelContext("Admin", "FakePage"));
-            expect(emitStub.calledWith('subscribe', {
+            client.verify(c => c.emit('subscribe', TypeMoq.It.isValue({
                 area: "Admin",
                 viewmodelId: "FakePage",
                 parameters: undefined
-            })).to.be(true);
+            })), TypeMoq.Times.once());
         });
 
         context("and custom parameters are needed on the backend side", () => {
             it("should also add these parameters to the subscription request", () => {
                 subject.notificationsFor(new ViewModelContext("Admin", "FakePage", {id: 60}));
-                expect(emitStub.calledWith('subscribe', {
+                client.verify(c => c.emit('subscribe', TypeMoq.It.isValue({
                     area: "Admin",
                     viewmodelId: "FakePage",
                     parameters: {id: 60}
-                })).to.be(true);
+                })), TypeMoq.Times.once());
             });
         });
     });
@@ -54,11 +43,11 @@ describe("NotificationManager, given an area and a viewmodel id", () => {
             let subscription = subject.notificationsFor(new ViewModelContext("Admin", "FakePage", {id: 60})).subscribe(_ => {
             });
             subscription.dispose();
-            expect(emitStub.calledWith('unsubscribe', {
+            client.verify(c => c.emit('unsubscribe', TypeMoq.It.isValue({
                 area: "Admin",
                 viewmodelId: "FakePage",
                 parameters: {id: 60}
-            })).to.be(true);
+            })), TypeMoq.Times.once());
         });
     });
 });
