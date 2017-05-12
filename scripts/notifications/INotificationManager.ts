233e12faff1cc4dce592ce256b4d595095fcdcbc
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-12 12:28:05 +0200

    Implement ModelRetriever from chupacabras

--- scripts/notifications/INotificationManager.ts
+++
@@ -1,9 +0,0 @@
-import Notification from "./Notification";
-import {ViewModelContext} from "ninjagoat";
-import {Observable} from "rx";
-
-interface INotificationManager {
-    notificationsFor(context: ViewModelContext): Observable<Notification>;
-}
-
-export default INotificationManager
