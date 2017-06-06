5b8809a794ad8aa4addc5652361a878221a47397
Mattia Franchetto <mt.franchetto@gmail.com>
2017-06-06 18:18:37 +0200

    Add registry parameters deserializer

---
+++ scripts/ParametersDeserializer.ts
@@ -0,0 +1,25 @@
+import {IParametersDeserializer, ModelContext} from "chupacabras";
+import {ViewModelContext, Dictionary} from "ninjagoat";
+import {multiInject, optional} from "inversify";
+import {map, zipObject} from "lodash";
+
+export interface IModelParametersProvider {
+    context: ViewModelContext;
+    provide(contextParameters: object): object;
+}
+
+export class ParametersDeserializer implements IParametersDeserializer {
+
+    private providers: Dictionary<IModelParametersProvider> = {};
+
+    constructor(@multiInject("IModelParametersProvider") @optional() parametersProvider: IModelParametersProvider[] = []) {
+        this.providers = zipObject<Dictionary<IModelParametersProvider>>(
+            map(parametersProvider, provider => `${provider.context.area}:${provider.context.viewmodelId}`),
+            parametersProvider);
+    }
+
+    deserialize(context: ModelContext): object {
+        let provider = this.providers[`${context.area}:${context.modelId}`];
+        return provider ? provider.provide(context.parameters) : null;
+    }
+}
